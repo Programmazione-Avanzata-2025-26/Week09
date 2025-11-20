f27e2c02caa4987983bcdef96fa7a14938cd1103
fabriziolamberti <41021126+fabriziolamberti@users.noreply.github.com>
2025-11-20 20:29:38 +0100

    Add files via upload
    Materiale aula 20 novembre 2025

--- UI/controller.py
+++ UI/controller.py
@@ -1,5 +1,4 @@
 import flet as ft
-
 from UI.view import View
 from model.model import Model
 
@@ -13,13 +12,31 @@ class Controller:
         # other attributes
         self._mese = 0
 
+
+
     def handle_umidita_media(self, e):
-        pass
-
-
+        mese = self._view.dd_mese.value
+        if mese == None:
+            self._view.create_alert("Scegli un mese")
+        else:
+            results = self._model.calcola_umidità_media(mese)
+            print(results)
+            self._view.lst_result.clean()
+            for result in results:
+                self._view.lst_result.controls.append(ft.Text(result[0]+" "+str(result[1])))
+            self._view.update_page()
 
     def handle_sequenza(self, e):
-        pass
+        mese = self._view.dd_mese.value
+        if mese == None:
+            self._view.create_alert("Scegli un mese")
+        else:
+            sequenza, costo = self._model.calcola_sequenza(mese)
+            self._view.lst_result.clean()
+            self._view.lst_result.controls.append(ft.Text(f"Il costo della soluzione è {costo}"))
+            for fermata in sequenza:
+                self._view.lst_result.controls.append(ft.Text(fermata))
+            self._view.update_page()
 
     def read_mese(self, e):
         self._mese = int(e.control.value)
--- model/model.py
+++ model/model.py
@@ -1,3 +1,84 @@
+import copy
+
+from database.meteo_dao import MeteoDao
+
 class Model:
     def __init__(self):
-        pass
+        # self._soluzioni = []
+        self._costo_minimo = -1
+        self._soluzione_ottima = []
+
+    def calcola_umidità_media(self, mese):
+        return MeteoDao.get_umidita_media_mese(mese)
+
+
+    def calcola_sequenza(self, mese):
+        situazioni_meta_mese = MeteoDao.get_situazione_meta_mese(mese)
+        self._ricorsione([], situazioni_meta_mese)
+        return self._soluzione_ottima, self._costo_minimo
+
+    def _ricorsione(self, parziale, situazioni):
+        # Caso/Condiz. terminale
+        if len(parziale) == 15:
+            print(parziale)
+            costo = self._calcola_costo(parziale)
+            if(self._costo_minimo==-1) or (costo < self._costo_minimo):
+                self._costo_minimo = costo
+                #self._soluzioni.append(copy.deepcopy(parziale))
+                self._soluzione_ottima = copy.deepcopy(parziale)
+
+        # Caso/cond. ricorsiva
+        else:
+            giorno = len(parziale)+1
+            #for situazione in situazioni: # 45 oggetti di tipo Situazione
+            # Altra ottimizzazione, lavorando su finestre di tre giorni rispetto al giorno considerato
+            for situazione in situazioni[(giorno-1)*3:giorno*3]:
+                if self.vincoli_soddisfatti(parziale, situazione):
+                    parziale.append(situazione)
+                    self._ricorsione(parziale, situazioni)
+                    parziale.pop()
+
+
+    def vincoli_soddisfatti(self, parziale, situazione):
+        # Vincolo su non più di 6 giornate (anche non consecutive) stessa località
+        contatore = 0
+        for fermata in parziale:
+            if fermata.localita == situazione.localita:
+                contatore+=1
+        if contatore>=6:
+            return False
+
+        # Vincolo su non più di tre giorni consecutivi stessa località
+        if len(parziale) >0 and len(parziale) <=2:
+            if situazione.localita != parziale[0].localita:
+                return False
+        elif len(parziale) > 2: # Se la sequenza ha almeno tre elementi occorre
+                                # verificare che il tecnico si sa fermato almeno
+                                #  tre giorni di fila nella stessa località
+            sequenza_finale = parziale[-3:] # Ultimi tre giorni in parziale
+            prima_fermata = sequenza_finale[0].localita # Località del primo di questi ultimi tre giorni
+            contatore = 0
+            for fermata in sequenza_finale: # Per ognuno di quei tre giorni
+                if fermata.localita == prima_fermata: # Se il tecnico si è fermato nella località in questione
+                    contatore += 1 # Incremento un contatore
+            # Se il contatatore è minore di 3, non si è fermato il tempo necessario
+            if (contatore < 3) and situazione.localita != sequenza_finale[-1].localita:
+                return False # Vincolo non soddisfatto
+
+        # Se tutti i vincoli sono soddisfatti
+        return True
+
+    def _calcola_costo(self, parziale):
+        costo = 0
+        for i in range(len(parziale)):
+            # Costo dell'uimità
+            costo += parziale[i].umidita
+
+            if i == 2: # primi due giorni
+                if (parziale[i].localita != parziale[0].localita):
+                    costo += 100
+            elif i > 2: # altri giorni
+                ultime_fermate = parziale[i-2:i+1]
+                if(ultime_fermate[2].localita != ultime_fermate[0].localita) or (ultime_fermate[2].localita != ultime_fermate[1].localita):
+                    costo += 100
+            return costo
